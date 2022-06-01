cd747d92e7435258ac87a38291ccef3b5d92a7cf
taoziG <thksdhr@163.com>
2022-06-02 00:15:56 +0800

    创建了第一个T形状的块并对齐到了创建的背景，ad左右移动，每0.8s自动向下移动（点击空格加快）-----未完成：不允许方块移动超过边界

--- Assets/Script/TetrisControl.cs
+++ Assets/Script/TetrisControl.cs
@@ -8,6 +8,8 @@ namespace Tetris
     public class TetrisControl : MonoBehaviour
     {
         private float IntervalsTime = 0.8f;//间隔时间
+        private float lastTime;//最后运动时间
+        private static int Width=9, Height=16;
         void Start()
         {
 
@@ -28,10 +30,22 @@ namespace Tetris
                 transform.position += new Vector3(1, 0, 0);
             }
             //自动向下移动或者手动向下移动
-            if (Time.time > IntervalsTime)
+            if (Time.time - lastTime > (Input.GetKeyDown(KeyCode.Space) ? IntervalsTime / 10 : IntervalsTime))//如果按下空格IntervalsTime/10，否则不改变
             {
                 transform.position += new Vector3(0, -1, 0);
+                lastTime = Time.time;
             }
+        }
+        //是否有效移动
+        private bool ValidMove()
+        {
+            foreach(Transform element in transform)//迭代所有对象的transform组件（组成这个块的所有小方块的transform组件）
+            {
+                //获取角色当前的取整XY值
+                int nowX = Mathf.RoundToInt(element.transform.position.x);
+                int nowY = Mathf.RoundToInt(element.transform.position.y);
+            }
+            return(false);
         }
     }
 }
